f9ced3accc008813892c69a61469e9163797d26e
AshACxx <irishjatt01@gmail.com>
2026-04-28 12:13:41 +0100

    Created sample data for ammonia

---
+++ SewagePlant/sewagedata.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+
+num_sens = 5
+num_samples = 1800
+np.random.seed(42)
+
+ammonia_samples = np.random.uniform(20,0,(num_sens, num_samples))
+for x, y in enumerate(ammonia_samples):
+    print(f"Sensor {x}: {y}")
+
+
+
+
+
+
+
+    
+    

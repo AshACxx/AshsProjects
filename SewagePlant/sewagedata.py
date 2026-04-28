1399d6c89e4d3aea0433866ce29518cb4fb07642
AshACxx <irishjatt01@gmail.com>
2026-04-28 12:14:24 +0100

    added zscore operations

--- SewagePlant/sewagedata.py
+++ SewagePlant/sewagedata.py
@@ -10,6 +10,10 @@ ammonia_samples = np.random.uniform(20,0,(num_sens, num_samples))
 for x, y in enumerate(ammonia_samples):
     print(f"Sensor {x}: {y}")
 
+mean = np.mean(ammonia_samples, axis = 1)
+std = np.std(ammonia_samples, axis = 1)
+
+zscore = (ammonia_samples - mean[:, np.newaxis]) / std[:, np.newaxis]
 
 
 

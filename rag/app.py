28c537e0930b6fee472df6fd128a6d7418830fb9
AshACxx <irishjatt01@gmail.com>
2026-09-06 21:15:43 +0100

    added pdf uploader to app, condition added if pdf if its not present

--- rag/app.py
+++ rag/app.py
@@ -14,3 +14,27 @@ st.title("AI Study Assistant")
 st.write(
     "Upload a PDF and ask questions about it."
 )
+
+# Let the user upload a PDF
+uploaded_file = st.file_uploader(
+    "Upload a PDF",
+    type="pdf"
+)
+
+
+if uploaded_file is not None:
+
+    # Streamlit gives us the uploaded file in memory.
+    # PyPDFLoader needs an actual file path,
+    # so we temporarily save the PDF.
+
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=".pdf"
+    ) as temp_file:
+
+        temp_file.write(
+            uploaded_file.getvalue()
+        )
+
+        temp_path = temp_file.name

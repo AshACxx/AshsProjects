98a2ec89564f63acf62eaf2f2036c721b44f5a36
AshACxx <irishjatt01@gmail.com>
2026-09-26 20:01:15 +0100

    committ

--- 07-heart-disease-analysis/analysis.py
+++ 07-heart-disease-analysis/analysis.py
@@ -3,12 +3,19 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import csv
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('07-heart-disease-analysis/data/heart.csv')
+checkChol = (df['chol'] == 0).sum()
+print(f"There are {checkChol} rows that equal to 0")
 
-print(df.head())
 
-print(df.isnull().sum())
+
+#print(df.head())
+
+#print(df.isnull().sum())
 
 sns.boxplot(data = df, x = 'sex', y = 'chol')
 plt.show()
